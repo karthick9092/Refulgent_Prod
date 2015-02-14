ca53ec44336de90f548a112c95aa605fee17e31b
karthick9092 <karthickmail.u@gmail.com>
2015-02-14 03:29:08 -0800

    ijiji

--- Core/src/main/java/com/refulgent/core/sample/repo/ProductRepository.java
+++ Core/src/main/java/com/refulgent/core/sample/repo/ProductRepository.java
@@ -1,27 +1,11 @@
 package com.refulgent.core.sample.repo;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.refulgent.entity.sample.Product;
 
-public class ProductRepository {
-	
-	@PersistenceContext
-	EntityManager em;
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
 
-	@SuppressWarnings("unchecked")
-	public List<Product> findAll() {
-		// TODO Auto-generated method stub
-		List<Product> productList = null;
-		try {
-			String sql = "select c from Product as c";
-			productList = em.createQuery(sql).getResultList();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return productList;
-	}	
 }
---
+++ Core/src/main/java/com/refulgent/core/sample/repo/ProductRepositoryl.java
@@ -0,0 +1,27 @@
+package com.refulgent.core.sample.repo;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.refulgent.entity.sample.Product;
+
+public class ProductRepositoryl {
+	
+	@PersistenceContext
+	EntityManager em;
+
+	@SuppressWarnings("unchecked")
+	public List<Product> findAll() {
+		// TODO Auto-generated method stub
+		List<Product> productList = null;
+		try {
+			String sql = "select c from Product as c";
+			productList = em.createQuery(sql).getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return productList;
+	}	
+}
